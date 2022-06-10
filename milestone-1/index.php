ab11338b6a4398ee98bf26a54c1f370e86925a4a
mohamedslima <hammaslima@gmail.com>
2022-06-10 14:31:29 +0200

    First commit

---
+++ milestone-1/index.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__ . "/../database.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dischi 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <div class="header__container">
+            <div class="header__logo_icon">LOGO SPOTIFY</div>
+        </div>
+    </header>
+    <main>
+        
+    </main>
+</body>
+
+</html>
